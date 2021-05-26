52cad951bddcab71d49b8e15e59e3e6d9adba2df
Appolinarry <polina-ineshina@mail.ru>
2021-05-26 16:29:18 +0800

    Переделана форма с таблицей результатов

--- src/sample/Controllers/TableWindowController.java
+++ src/sample/Controllers/TableWindowController.java
@@ -40,6 +40,19 @@ public class TableWindowController {
     private TableColumn<Person, Integer> GameCount;
 
     @FXML
+    private TableColumn<Person, String> Title;
+
+    @FXML
+    private TableColumn<Person, String> Comment;
+
+    @FXML
+    private TableColumn<Person, Integer> LifeCount;
+
+    @FXML
+    private TableColumn<Person, Integer> OurCount;
+
+
+    @FXML
     void ReversButtonAction(ActionEvent event) {
         ReversButton.getScene().getWindow().hide();
         Stage primaryStage = new Stage();
@@ -57,13 +70,34 @@ public class TableWindowController {
 
     @FXML
     void initialize() throws SQLException, ClassNotFoundException {
-        //BDPerson.readDB();
+        BDPerson.readDB();
         PersonData = BDPerson.getPersonData();
         NumberPerson.setCellValueFactory(new PropertyValueFactory<Person,Integer>("NumberPerson"));
         Nicname.setCellValueFactory(new PropertyValueFactory<Person, String>("Nicname"));
         NameGame.setCellValueFactory(new PropertyValueFactory<Person, String>("NameGame"));
         GameCount.setCellValueFactory(new PropertyValueFactory<Person, Integer>("GameCount"));
+        Title.setCellValueFactory(new  PropertyValueFactory<Person,String>("Title"));
+        Comment.setCellValueFactory(new PropertyValueFactory<Person,String>("Comment"));
+        LifeCount.setCellValueFactory(new PropertyValueFactory<Person,Integer>("LifeCount"));
+        OurCount.setCellValueFactory(new PropertyValueFactory<Person,Integer>("OurCount"));
         // Заполняем таблицу данными
         RecordCount.setItems(PersonData);
+        ReversButton.setOnAction(e->{
+            Stage stage = (Stage) ReversButton.getScene().getWindow();
+            stage.close();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/Windows/sample.fxml"));
+            try {
+                loader.load();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("TableMenu");
+            stage.setResizable(false);
+            stage.show();
+        });
     }
 }
